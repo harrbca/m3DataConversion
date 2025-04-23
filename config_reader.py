7d6aefcc27191031d2b8fd63c9b564fd4bf2c29c
Brad Harrison <bharrison@buckwold.com>
2025-04-23 13:52:13 -0600

    adjust config_reader to search for custom config files.

--- config_reader.py
+++ config_reader.py
@@ -1,23 +1,30 @@
 import configparser
 import os
+import glob
 
 class ConfigReader:
     _instance = None
 
-    def __init__(self, config_file="./config/config.ini", credentials_file="./config/credentials.ini"):
+    def __init__(self, config_file=None, credentials_file="./config/credentials.ini"):
         if hasattr(self, "config"):
             return
-        self.config_file = config_file
+        self.config_file = config_file or self._find_config_file()
         self.credentials_file = credentials_file
         self.config = configparser.ConfigParser()
         self.credentials = configparser.ConfigParser()
         self.load_config()
 
     @staticmethod
-    def get_instance(config_file="./config/config.ini"):
+    def get_instance(config_file=None):
         if ConfigReader._instance is None:
             ConfigReader._instance = ConfigReader(config_file)
         return ConfigReader._instance
+
+    def _find_config_file(self):
+        matches = glob.glob("custom/*/config/config.ini", recursive=True)
+        if not matches:
+            raise FileNotFoundError("No config.ini file found in custom/*/config/")
+        return matches[0]
 
     def load_config(self):
         if not os.path.exists(self.config_file):
