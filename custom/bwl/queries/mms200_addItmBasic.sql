f7025780491f7cfee8a8d23bf978bcb316779ae8
Brad Harrison <bharrison@buckwold.com>
2025-04-30 11:10:48 -0600

    modify active item query for MMS200

--- custom/bwl/queries/mms200_addItmBasic.sql
+++ custom/bwl/queries/mms200_addItmBasic.sql
@@ -1,4 +1,4 @@
 select * from items
 left join main.item_hierarchy ih on items.ITEMNUMBER = ih.ITEMNUMBER
-where imfgr not in ('ARM', 'CAS', 'PHE', 'INT', 'JON', 'ALA', 'TAN') and items.ITEMNUMBER  not like '%MISC%'
+where imfgr not in ('INT', 'JON', 'ALA', 'TAN') and items.ITEMNUMBER  not like '%MISC%'
 and iprodl not in ('SAM', 'SET', 'DIS')
